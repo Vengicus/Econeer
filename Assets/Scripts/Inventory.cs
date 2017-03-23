b5613a501b71c0e32a6d56ad917045d11da9cba9
Vengicus <andrew.schoolnick@gmail.com>
2017-03-23 08:40:13 -0400

    Testing commits

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour 
 {
-	public void Init()
+	private int cost, happiness, energy, people;
+	private string name, description;
+	private GameObject obj;
+	private Texture2D icon;
+	public void Init(int cost, int happiness, int energy, int people, string name, string desc, GameObject obj, Texture2D icon)
 	{
-
+		this.cost = cost;
+		this.happiness = happiness;
+		this.energy = energy;
+		this.people = people;
+		this.name = name;
+		this.description = desc;
+		this.obj = obj;
+		this.icon = icon;
 	}
 	public virtual void buildPrefab()
 	{
