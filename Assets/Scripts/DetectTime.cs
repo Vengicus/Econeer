6c8190f4f7d511f36f5e03001c387ef1d24fd2a2
Administrator <admin@example.com>
2017-04-16 17:25:40 -0400

    Added sat rotation and timer

---
+++ Assets/Scripts/DetectTime.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DetectTime : MonoBehaviour {
+
+	float seconds;
+	float minutes;
+	//float hours;
+	public Text timePlayed;
+
+	void Update(){
+
+		seconds += Time.deltaTime;
+
+		if (seconds >= 60) {
+			minutes += 1;
+			seconds = 0;
+		}
+
+		if (minutes >= 60) {
+			//hours += 1;
+			minutes = 0;
+		}
+		//timePlayed.text = ("M: " + minutes + " S: " + (int)seconds);
+		timePlayed.text = (minutes + ":" + (int)seconds);
+		//Debug.Log ("H: " + hours + " M: " + minutes + " S: " + (int)seconds);
+	}
+}
---
+++ Assets/Scripts/rotate.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+var target : Transform;
+
+var orbitSpeed : float = 10.0;
+
+function Start () {
+	
+}
+
+function Update () {
+	transform.RotateAround(target.position, Vector3.up,orbitSpeed * Time.deltaTime);
+}
